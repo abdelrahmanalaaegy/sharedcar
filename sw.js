104e88d60d63b375933a07cd9f3edc15f3d772f2
abdelrahmanalaaegy <abdelrahmanalaaegy@gmail.com>
2026-08-01 22:23:16 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('ShareRide Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // هذا الكود يجعل التطبيق يعمل بسلاسة
-});
