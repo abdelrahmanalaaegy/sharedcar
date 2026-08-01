7b8bebec74618f8fc0a565161d0acb1b43281710
abdelrahmanalaaegy <abdelrahmanalaaegy@gmail.com>
2026-08-01 08:07:44 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('ShareRide Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // هذا الكود يجعل التطبيق يعمل بسلاسة
+});
