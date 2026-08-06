d961fdfee7e55146b9bbab5a1f7186d04b96f152
abdelrahmanalaaegy <abdelrahmanalaaegy@gmail.com>
2026-08-06 05:39:53 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,7 @@
-export const SB_URL = "https://owzaevvsvwufozynzvin.supabase.co"; 
-export const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93emFldnZzdnd1Zm96eW56dmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDQ4NTgsImV4cCI6MjEwMTA4MDg1OH0.LRd_ekWBrk_BWDPv1PSmpSD9RygerdeeUCHUOQlwukQ";
+const SB_URL = "https://owzaevvsvwufozynzvin.supabase.co"; 
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93emFldnZzdnd1Zm96eW56dmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDQ4NTgsImV4cCI6MjEwMTA4MDg1OH0.LRd_ekWBrk_BWDPv1PSmpSD9RygerdeeUCHUOQlwukQ";
 
-export const sb = window.supabase.createClient(SB_URL, SB_KEY);
+const sb = supabase.createClient(SB_URL, SB_KEY);
+window.sb = sb;
+window.SB_URL = SB_URL;
+window.SB_KEY = SB_KEY;
