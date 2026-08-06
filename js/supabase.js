c1e71eb85290855047d5372321268dc8ae1abee6
abdelrahmanalaaegy <abdelrahmanalaaegy@gmail.com>
2026-08-06 05:01:42 +0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+export const SB_URL = "https://owzaevvsvwufozynzvin.supabase.co"; 
+export const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93emFldnZzdnd1Zm96eW56dmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDQ4NTgsImV4cCI6MjEwMTA4MDg1OH0.LRd_ekWBrk_BWDPv1PSmpSD9RygerdeeUCHUOQlwukQ";
+
+export const sb = window.supabase.createClient(SB_URL, SB_KEY);
